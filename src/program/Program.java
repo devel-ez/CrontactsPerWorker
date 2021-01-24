f51a3d30bc78bdb777d789803e258f56db0beea5
devel-ez <felipevelez.adm@hotmail.com>
2021-01-24 14:17:53 -0300

    Incluir data com SimpleDateFormat

--- src/program/Program.java
+++ src/program/Program.java
@@ -8,6 +8,9 @@ package program;
 import entities.Departament;
 import entities.Worker;
 import entities.enums.WorkerLevel;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -15,10 +18,11 @@ import java.util.Scanner;
 class Program {
 
     
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
        
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
+        SimpleDateFormat sdf = new SimpleDateFormat ("dd/MM/yyyy");    
         
         System.out.print("Enter departament's name: ");
         String departamentName = sc.nextLine();
@@ -31,8 +35,15 @@ class Program {
         double baseSalary = sc.nextDouble();
         Worker worker =  new Worker( workerName,WorkerLevel.valueOf(workerLevel), baseSalary, new Departament (departamentName) );
         
+        System.out.println("How many conracts to this worker?  ");
+        int n = sc.nextInt();
         
-        
+        for (int i =1; i<=n;i++){
+            System.out.println("Enter contract #" + i + "data:");
+            System.out.print("Date DD/MM/YYYY: ");
+            Date contractdate =  sdf.parse(sc.next());
+            /*sadfasdfsafsdfsadfsaf*/
+        }
         sc.close();
     }
     
